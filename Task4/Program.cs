d0b59b24a94802356c4b58e72bff09e62809a73d
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 21:55:11 +0300

    Решена задача с двойным условием для if

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23: 14 - нет, 161 - да
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA=int.Parse(Console.ReadLine());
+Console.WriteLine(numberA % 7 == 0 && numberA % 23 == 0 ? "Кратно" : "Не кратно");
