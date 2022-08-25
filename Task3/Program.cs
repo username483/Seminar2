9e52ce12ab7b44ba121de4b0a2784d7cf489870f
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 21:34:46 +0300

    Решена задача на вывод 2 чисел, второе кратно первому, остаток

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая берет на вход 2 числа
+//и выдает, является ли второе число кратным первому
+//Если число 2 не кратно первому, выводится остаток от деления
+// 34,5 - не кратно, остаток 4; 16б4 - кратно
+
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int numberA=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB=int.Parse(Console.ReadLine());
+int numberC=numberA%numberB;
+if (numberC==0)
+{
+    Console.WriteLine($"{numberA}, {numberB} - кратно");
+}
+else
+{
+    Console.WriteLine($"{numberA}, {numberB} - не кратно. остаток {numberC}");
+}
