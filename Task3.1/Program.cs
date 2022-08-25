7578176b911b5c90aa1e1d0058b8e50e3b04a01f
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 21:47:42 +0300

    второй вариант решения задачи с выводом числа, делителя и остатка - в 1 строку

---
+++ Task3.1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число ");
+int numberA=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB=int.Parse(Console.ReadLine());
+Console.WriteLine(numberA % numberB == 0 ? "Кратно" : $"Не кратно, {numberA%numberB}");
