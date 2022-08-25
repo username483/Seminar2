1b68f29a32ab316101a9f94775b312a72135610e
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 22:07:18 +0300

    решена задача 1число квадрат другого с оператором или

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого.
+// 5, 25 -> да
+//25, 5 -> да
+//8,9 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите первое число ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine(A % B == 0 || B % A == 0 ? "да" : "нет");
