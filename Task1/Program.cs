3f7261353b69d8c6d3a55ca86523b47b4ece8528
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-25 21:04:00 +0300

    решена задача на вывод максимального знака двузначного числа и задача на вывод 1 и 3 десятка 3-значного числа

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10,99] 
+//и показывает наибольшую цтфру этого числа
+// Например 78 - 8
+Console.Clear();
+int numberA = new Random().Next(10,100);
+int numberB = numberA/10;
+int numberC = numberA%10;
+if (numberC>numberB)
+{
+    Console.WriteLine($"{numberA}, {numberC}");
+}
+else
+{
+Console.WriteLine($"{numberA}, {numberB}");
+}
